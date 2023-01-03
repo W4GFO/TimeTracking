b117a9e6b904c800494cd1f685d08bdd0b9a2740
Jeff Behrbaum <sejapresident@gmail.com>
2023-01-03 14:24:50 -0500

    Got the initial alignment of the user card set up.

--- time-tracker/src/Components/Cards/UserCard.tsx
+++ time-tracker/src/Components/Cards/UserCard.tsx
@@ -1,5 +1,6 @@
 import './Styles/UserCardStyle.scss'
 import {TIMEFRAME} from '../../Utils/defs'
+import IMG from '../../Images/image-jeremy.png'
 
 type UserCardProps = {
 	className:string
@@ -12,8 +13,14 @@ export const UserCard = ({className, funcSetNewTimeFrame}: UserCardProps) => {
 	return (
 		<div className={userCardClassName}>
 			<section className='user-card-person'>
-				<p>Report for:</p>
-				<p>Jeremy Robson</p>
+				<div className='user-card-person-imageinfo'>
+					<img src={IMG}/>
+				</div>
+
+				<div className='user-card-person-textinfo'>
+					<p>Report for:</p>
+					<p>Jeremy Robson</p>
+				</div>
 			</section>
 			<section className='user-card-timeframes-frame'>
 				<ul className='user-card-list-of-timeframes'>
