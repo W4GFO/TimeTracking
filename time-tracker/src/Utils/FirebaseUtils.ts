677d61addf92bac8860a75d56ab651b2b3de4641
Jeff Behrbaum <sejapresident@gmail.com>
2023-01-04 12:25:50 -0500

    Using a local env file instead of publishing credentials to a public repo.

--- time-tracker/src/Utils/FirebaseUtils.ts
+++ time-tracker/src/Utils/FirebaseUtils.ts
@@ -7,13 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "--",
-  authDomain: "--",
-  projectId: "--",
-  storageBucket: "--",
-  messagingSenderId: "--",
-  appId: "--",
-  measurementId: "--"
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	//databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJ_ID,
+	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
